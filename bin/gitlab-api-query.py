1da4065623f7883dca8377aab1f5af63a510d3f3
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2018-10-26 14:01:22 +0300

    Update gitlab-api-query to gitlab V4 API

--- bin/gitlab-api-query.py
+++ bin/gitlab-api-query.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 import sys, requests, os.path
-try:
-    from urllib.parse import quote_plus
-except ImportError:
-    from urllib import quote_plus
+from urllib.parse import urlencode
 
 
 SOURCE_FILE = "/submission/user/gitsource"
@@ -17,33 +13,49 @@ def main():
     gitlab_api_query(*sys.argv[1:])
 
 
+class Gitlab:
+    def __init__(self, host, token = None):
+        self.host = host
+        self.token = token
+
+    def get(self, path, args):
+        safe_url = "https://{}/api/v4/{}?{}".format(self.host, path, urlencode(args))
+        if self.token:
+            args['private_token'] = self.token
+        url = "https://{}/api/v4/{}?{}".format(self.host, path, urlencode(args))
+        r = requests.get(url, timeout=3)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            r.url = safe_url
+            r.raise_for_status()
+
 def gitlab_api_query(host, token = None, forked = None):
     source = read_file(SOURCE_FILE).strip()
-    url = None
+    gitlab = Gitlab(host, token)
     try:
-        rid = quote_plus(source[source.index(":") + 1:-4])
-        url = "https://{}/api/v3/projects/{}".format(host, rid)
-        qs = "?private_token={}".format(token)
+        rid = source.rpartition(':')[2][:-4]
+        username, project_path = rid.split('/', 1)
 
-        r = requests.get(url + (qs if token else ''), timeout=3)
-        if r.status_code == 200:
-            gitlab_api_check(r.json(), forked)
-        elif r.status_code == 404:
-            if token:
-                print("Did not find project:\n{}".format(url), file=sys.stderr)
-                sys.exit(1)
-        else:
-            r.raise_for_status()
+        users = gitlab.get('users', dict(username=username))
+        if len(users) != 1:
+            raise RuntimeError("Couldn't find user {!r}".format(username))
+        uid = users[0]['id']
+
+        projects = gitlab.get('users/{}/projects'.format(uid), dict(path=project_path))
+        if len(projects) != 1:
+            raise RuntimeError("Couldn't find project {!r} from user {!r}. Do we have access?".format(project_path, username))
+
+        gitlab_api_check(projects[0], forked)
     except Exception as e:
-        print("Failed to check project:\n{}".format(url), file=sys.stderr)
+        print("Failed to check project {!r}".format(source), file=sys.stderr)
         print(str(e), file=sys.stderr);
         sys.exit(1)
 
 
 def gitlab_api_check(json, forked = None):
-    if json.get("public", True):
-        print("{}\nhas public access in settings!\n"
-            "Remove it to grade exercises."
+    if json.get("visibility") != 'private':
+        print("{}\nProject is not private! Change visibility to private in project settings for submission to be graded!"
             .format(json["web_url"]), file=sys.stderr)
         sys.exit(1)
     if forked:
